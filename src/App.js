1d1546e5679b0c8bb542bd828b29446fddc3fe3e
grif1t <nurzhaumurzaliev08@gmail.com>
2022-06-19 17:08:29 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import {format} from 'date-fns';
 import './App.css';
 
-function App() {
+
+const App = () =>{
+  const date = format(new Date(), "dd.MM.yy HH:MM");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+      <div className='header'>
+          <div className='icon'></div>
+          <div className='box'></div>
+          <div className='timeTable'>{date}</div>
+      </div>
+      <div className= 'container'>
+      <div className='imgBox' id = "Antarctic">
+          <div className="text"> HEY</div>
+      </div>
+
+      <div className='imgBox' id = "flashlight">
+          <div className="text"> LET'S </div>
+      </div>
+
+      <div className='imgBox' id = "women">
+          <div className="text"> GIVE </div>
+      </div>
+
+      <div className='imgBox' id = "foxpic">
+          <div className="text"> ALL </div>  
+      </div>
+      <div className='imgBox' id = "clouds">
+          <div className="Text"> YOU CAN</div>
+      </div>
+    </div >
     </div>
   );
-}
+};
 
 export default App;
